e3bd29ea98836355429d509e83f2b38459befd68
Sora Nakao <6051_2023_05523@g.miyazaki-c.ed.jp>
2025-03-12 05:22:16 +0000

    11

--- db/select.sql
+++ db/select.sql
@@ -1,5 +1,8 @@
 -- 中学校テーブルからすべてのデータを抽出する
-select * from schools;
+-- select * from schools;
 
--- 生徒テーブル（students）からすべてのデータ（*）を抽出する
-select * from students;
+-- 生徒テーブル（students）から性別が'女'のデータ（*）を抽出する
+-- select name, gender, address from students where gender='女';
+
+
+SELECT name, gender, school_name FROM students LEFT JOIN schools ON students.school_id = schools.school_id;
